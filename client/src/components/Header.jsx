a7e47d2eeda6f010d10a8accf38f367553397e25
kbrown <kamauri205@gmail.com>
2024-03-05 23:10:21 -0500

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,20 +7,15 @@ export default function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Sahand</span>
+            <span className="text-slate-500">Mandeville</span>
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
-        <form
-          //   onSubmit={handleSubmit}
-          className="bg-slate-100 p-3 rounded-lg flex items-center"
-        >
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
-            // value={searchTerm}
-            // onChange={(e) => setSearchTerm(e.target.value)}
           />
           <button>
             <FaSearch className="text-slate-600" />
